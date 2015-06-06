296d8e88a50ac8d81697f830dbbe65108f80fb9c
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-07 01:41:22 +0200

    Generate symfony console classes

---
+++ tests/unit/Support/Console/SymfonyInputSpec.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace unit\Acme\Support\Console;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class SymfonyInputSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Acme\Support\Console\SymfonyInput');
+    }
+}
