8edd279d7cc9e7302fa973c724a83495b21e9e4b
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-06 12:33:35 +0200

    Add abstract methods

--- php/Support/Container/Container.php
+++ php/Support/Container/Container.php
@@ -29,4 +29,49 @@ abstract class Container
      * @return Container
      */
     abstract public function add($alias, $concrete = null, $singleton = false);
+
+    /**
+     * Add a singleton definition to the container.
+     *
+     * @param  string $alias
+     * @param  mixed $concrete
+     * @return Container
+     */
+    abstract public function singleton($alias, $concrete = null);
+
+    /**
+     * Allows for methods to be invoked on any object
+     * that is resolved of the type provided.
+     *
+     * @param  string $type
+     * @param  callable $callback
+     * @return Container
+     */
+    abstract public function inflector($type, callable $callback = null);
+
+    /**
+     * Get an item from the container.
+     *
+     * @param  string $alias
+     * @param  array $args
+     * @return mixed
+     */
+    abstract public function get($alias, array $args = []);
+
+    /**
+     * Invoke.
+     *
+     * @param  mixed $alias
+     * @param  array $args
+     * @return mixed
+     */
+    abstract public function call($alias, array $args = []);
+
+    /**
+     * Check if an item is registered in the container.
+     *
+     * @param  string $alias
+     * @return boolean
+     */
+    abstract public function exists($alias);
 }
