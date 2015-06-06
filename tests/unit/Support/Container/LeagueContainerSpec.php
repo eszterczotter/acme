ff546934ca374d9285719edb1ba90cbe39e29dd9
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-06 11:37:44 +0200

    Remove unncessary test

--- tests/unit/Support/Container/LeagueContainerSpec.php
+++ tests/unit/Support/Container/LeagueContainerSpec.php
@@ -7,19 +7,9 @@ use Prophecy\Argument;
 
 class LeagueContainerSpec extends ObjectBehavior
 {
-    function let()
-    {
-        $this->beConstructedThrough('instance', []);
-    }
-
     function it_is_initializable()
     {
         $this->shouldHaveType('Acme\Support\Container\Container');
-    }
-
-    function it_is_a_singleton()
-    {
-        $this->instance()->shouldBe($this);
     }
 
     function it_gets_itself()
