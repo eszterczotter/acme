b8e3ffc829c81e1947f2711ab9d0d5e0d8449fbc
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-06 11:58:45 +0200

    Refactor

--- php/Support/Container/LeagueContainer.php
+++ php/Support/Container/LeagueContainer.php
@@ -22,7 +22,7 @@ class LeagueContainer extends Container
     }
 
     /**
-     * Add a definition to the container
+     * Add a definition to the container.
      *
      * @param string $alias
      * @param mixed $concrete
@@ -36,68 +36,34 @@ class LeagueContainer extends Container
     }
 
     /**
-     * Adds a service provider to the container
-     *
-     * @param  string|\League\Container\ServiceProvider $provider
-     * @return ContainerInterface
-     */
-    public function addServiceProvider($provider)
-    {
-        return $this->container->addServiceProvider($provider);
-    }
-
-    /**
-     * Add a singleton definition to the container
+     * Add a singleton definition to the container.
      *
      * @param  string $alias
      * @param  mixed $concrete
-     * @return \League\Container\Definition\DefinitionInterface|ContainerInterface
+     * @return Container
      */
     public function singleton($alias, $concrete = null)
     {
-        return $this->container->singleton($alias, $concrete);
+        $this->container->singleton($alias, $concrete);
+        return $this;
     }
 
     /**
-     * Allows for methods to be invoked on any object that is resolved of the tyoe
-     * provided
+     * Allows for methods to be invoked on any object
+     * that is resolved of the type provided.
      *
      * @param  string $type
      * @param  callable $callback
-     * @return \League\Container\Inflector|void
+     * @return Container
      */
     public function inflector($type, callable $callback = null)
     {
-        return $this->container->inflector($type, $callback);
+        $this->container->inflector($type, $callback);
+        return $this;
     }
 
     /**
-     * Add a callable definition to the container
-     *
-     * @param  string $alias
-     * @param  callable $concrete
-     * @return \League\Container\Definition\DefinitionInterface
-     */
-    public function invokable($alias, callable $concrete = null)
-    {
-        return $this->container->invokable($alias, $concrete);
-    }
-
-    /**
-     * Modify the definition of an already defined service
-     *
-     * @param   string $alias
-     * @throws  \InvalidArgumentException if the definition does not exist
-     * @throws  \League\Container\Exception\ServiceNotExtendableException if service cannot be extended
-     * @return  \League\Container\Definition\DefinitionInterface
-     */
-    public function extend($alias)
-    {
-        return $this->container->extend($alias);
-    }
-
-    /**
-     * Get an item from the container
+     * Get an item from the container.
      *
      * @param  string $alias
      * @param  array $args
@@ -109,9 +75,9 @@ class LeagueContainer extends Container
     }
 
     /**
-     * Invoke
+     * Invoke.
      *
-     * @param  string $alias
+     * @param  mixed $alias
      * @param  array $args
      * @return mixed
      */
@@ -121,35 +87,13 @@ class LeagueContainer extends Container
     }
 
     /**
-     * Check if an item is registered with the container
+     * Check if an item is registered in the container.
      *
      * @param  string $alias
      * @return boolean
      */
-    public function isRegistered($alias)
+    public function exists($alias)
     {
         return $this->container->isRegistered($alias);
-    }
-
-    /**
-     * Check if an item is being managed as a singleton
-     *
-     * @param  string $alias
-     * @return boolean
-     */
-    public function isSingleton($alias)
-    {
-        return $this->container->isSingleton($alias);
-    }
-
-    /**
-     * Determines if a definition is registered via a service provider.
-     *
-     * @param  string $alias
-     * @return boolean
-     */
-    public function isInServiceProvider($alias)
-    {
-        return $this->container->isInServiceProvider($alias);
     }
 }
