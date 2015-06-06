f3ed61b62c2104e50253d42169870e77b3b1a4a0
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-06 09:59:19 +0200

    Use symfony in cli application

--- php/Application/Cli/Application.php
+++ php/Application/Cli/Application.php
@@ -2,10 +2,13 @@
 
 namespace Acme\Application\Cli;
 
+use Symfony\Component\Console\Application as Console;
+
 class Application extends \Acme\Application\Application
 {
     public function run()
     {
-        echo "Console Application 0.0.0\n";
+        $console = new Console("Console Application", "0.0.0");
+        $console->run();
     }
 }
