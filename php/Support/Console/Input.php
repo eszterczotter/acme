44c31d54cd07d192b3847734c75c8b72731ca0e8
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-06 21:29:21 +0200

    Dream up input and output interfaces

--- php/Support/Console/Input.php
+++ php/Support/Console/Input.php
@@ -5,4 +5,60 @@ namespace Acme\Support\Console;
 interface Input
 {
 
+    /**
+     * Get the script arguments.
+     *
+     * @return array
+     */
+    public function arguments();
+
+    /**
+     * Get an argument.
+     * @param string $name
+     * @return string
+     */
+    public function argument($name);
+
+    /**
+     * Get input from user.
+     *
+     * @param string $question
+     * @param string|null $default
+     * @return string
+     */
+    public function input($question, $default = null);
+
+    /**
+     * Get yes or no from user.
+     *
+     * @param string $question
+     * @return boolean
+     */
+    public function confirm($question);
+
+    /**
+     * Get password from user.
+     *
+     * @param string $question
+     * @return string
+     */
+    public function password($question);
+
+    /**
+     * Get multiple selection from user.
+     *
+     * @param string $question
+     * @param array $options
+     * @return array
+     */
+    public function select($question, $options);
+
+    /**
+     * Get single selection from user.
+     *
+     * @param string $question
+     * @param array $options
+     * @return string
+     */
+    public function choose($question, $options);
 }
--- php/Support/Console/Output.php
+++ php/Support/Console/Output.php
@@ -4,5 +4,100 @@ namespace Acme\Support\Console;
 
 interface Output
 {
+    /**
+     * Output error.
+     *
+     * @param string $text
+     * @return Output
+     */
+    public function error($text);
 
+    /**
+     * Output comment.
+     *
+     * @param string $text
+     * @return Output
+     */
+    public function comment($text);
+
+    /**
+     * Output information.
+     *
+     * @param string $text
+     * @return Output
+     */
+    public function info($text);
+
+    /**
+     * Output table.
+     *
+     * @param string $data
+     * @return Output
+     */
+    public function table($data);
+
+
+    /**
+     * Output json.
+     *
+     * @param string $data
+     * @return Output
+     */
+    public function json($data);
+
+    /**
+     * Output columns.
+     *
+     * @param string $data
+     * @return Output
+     */
+    public function columns($data);
+
+
+    /**
+     * Output horizontal line.
+     *
+     * @param string $pattern
+     * @param integer $length
+     * @return Output
+     */
+    public function hr($pattern, $length);
+
+    /**
+     * Output table.
+     *
+     * @param string $bar
+     * @param integer $progress
+     * @return Output
+     */
+    public function progress($bar, $progress);
+
+    /**
+     * Output variable
+     *
+     * @param mixed $variable
+     * @return Output
+     */
+    public function dump($variable);
+
+    /**
+     * Output line break.
+     *
+     * @return Output
+     */
+    public function br();
+
+    /**
+     * Output tabulator.
+     *
+     * @return Output
+     */
+    public function tab();
+
+    /**
+     * Clear output.
+     *
+     * @return Output
+     */
+    public function clear();
 }
