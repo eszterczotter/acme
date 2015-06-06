6803d4d9d2ea6862df6c674dc0031739016bec30
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-06 17:55:35 +0200

    Register config service provider

--- bootstrap/services.php
+++ bootstrap/services.php
@@ -4,4 +4,6 @@
  * @var \Acme\Support\Container\Container $container
  */
 
+$container->register('Acme\Support\Config\ConfigServiceProvider');
+
 $container->register('Acme\Support\Console\ConsoleServiceProvider');
