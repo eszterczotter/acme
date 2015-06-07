a7bdc5d71019ca031ca58c25de258b4d69ef5a60
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-07 18:44:22 +0200

    Implement handler interface

--- php/Support/Debug/ExceptionHandler.php
+++ php/Support/Debug/ExceptionHandler.php
@@ -2,6 +2,39 @@
 
 namespace Acme\Support\Debug;
 
-class ExceptionHandler
+use Acme\Support\Container\Container;
+use League\BooBoo\Handler\HandlerInterface;
+use League\BooBoo\Handler\LogHandler;
+
+class ExceptionHandler implements HandlerInterface
 {
+    /**
+     * The Container instance.
+     *
+     * @var Container
+     */
+    private $container;
+    /**
+     * The Booboo log handler.
+     *
+     * @var LogHandler
+     */
+    private $logHandler;
+
+    /**
+     * Create new Exception Handler.
+     *
+     * @param Container $container
+     * @param LogHandler $logHandler
+     */
+    public function __construct(Container $container, LogHandler $logHandler)
+    {
+        $this->container = $container;
+        $this->logHandler = $logHandler;
+    }
+
+    public function handle(\Exception $e)
+    {
+        // TODO: Implement handle() method.
+    }
 }
--- tests/unit/Support/Debug/ExceptionHandlerSpec.php
+++ tests/unit/Support/Debug/ExceptionHandlerSpec.php
@@ -2,13 +2,21 @@
 
 namespace unit\Acme\Support\Debug;
 
+use Acme\Support\Container\Container;
+use League\BooBoo\Handler\LogHandler;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class ExceptionHandlerSpec extends ObjectBehavior
 {
+    function let(Container $container, LogHandler $logHandler)
+    {
+        $this->beConstructedWith($container, $logHandler);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Acme\Support\Debug\ExceptionHandler');
+        $this->shouldHaveType('League\BooBoo\Handler\HandlerInterface');
     }
 }
