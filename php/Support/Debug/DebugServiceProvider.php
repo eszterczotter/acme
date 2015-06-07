a44a9a1d7743ac926f15de3228fe407e6a54fd85
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-07 19:49:01 +0200

    Generate debug service provider

---
+++ php/Support/Debug/DebugServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Acme\Support\Debug;
+
+use Acme\Support\Container\ServiceProvider;
+
+class DebugServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the services of this provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+
+    /**
+     * The services of this provider.
+     *
+     * @return array
+     */
+    public function services()
+    {
+        // TODO: Implement services() method.
+    }
+}
