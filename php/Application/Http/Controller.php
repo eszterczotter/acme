4a91a0b63cd666237ab9513109b5771e263590af
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-20 09:02:07 +0200

    Make the command bus usage easy

--- php/Application/Http/Controller.php
+++ php/Application/Http/Controller.php
@@ -9,11 +9,15 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 class Controller
 {
     /**
+     * The current Request value object.
+     *
      * @var Request
      */
     protected $request;
 
     /**
+     * The current Response value object.
+     *
      * @var Response
      */
     protected $response;
@@ -24,6 +28,8 @@ class Controller
     private $container;
 
     /**
+     * Set the Request.
+     *
      * @param Request $request
      */
     public function setRequest(Request $request)
@@ -32,6 +38,8 @@ class Controller
     }
 
     /**
+     * Set the Response.
+     *
      * @param Response $response
      */
     public function setResponse(Response $response)
@@ -39,20 +47,76 @@ class Controller
         $this->response = $response;
     }
 
+    /**
+     * Set the Container.
+     *
+     * @param Container $container
+     */
     public function setContainer(Container $container)
     {
         $this->container = $container;
     }
 
-    protected function respond($content)
+    /**
+     * Respond with the given string.
+     *
+     * @param string|null $content
+     * @return Response
+     */
+    protected function respond($content = null)
     {
+        if (is_null($content)) {
+            return $this->response;
+        }
+
         $this->response->getBody()->write($content);
-        return $this->response;
+        return $this->respond();
     }
 
+    /**
+     * Respond with the given view.
+     *
+     * @param string $template
+     * @param array $data
+     * @return Response
+     */
     protected function view($template, $data = [])
     {
         $view = $this->container->get('view');
         return $this->respond($view->render($template, $data));
+    }
+
+    /**
+     * Execute a command.
+     *
+     * @param string|object $command
+     * @param array|null $data
+     * @return mixed
+     */
+    protected function execute($command, array $data = null)
+    {
+        if (is_string($command)) {
+            $command = $this->resolveCommand($command, $data);
+        }
+
+        $bus = $this->container->get('bus');
+
+        return $bus->execute($command);
+    }
+
+    /**
+     * Resolve the command out of the Container.
+     *
+     * @param string $command
+     * @param array|null $data
+     * @return mixed
+     */
+    private function resolveCommand($command, array $data = null)
+    {
+        if (!is_null($data)) {
+            return $this->container->get($command, $data);
+        } else {
+            return $this->container->get($command, $this->request->getParsedBody());
+        }
     }
 }
