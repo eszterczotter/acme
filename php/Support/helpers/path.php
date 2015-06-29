49b24ab752605f8fa00173e47c4a5857cdf6c5ce
Eszter Czotter <eszter.czotter@gmail.com>
2015-06-29 20:23:41 +0200

    Simplify path function

--- php/Support/helpers/path.php
+++ php/Support/helpers/path.php
@@ -7,38 +7,6 @@ if (! function_exists('path')) {
     {
         $app = Container::instance()->get('app');
 
-        if ($dir == 'base') {
-            return $app->basePath();
-        }
-
-        if ($dir == 'boot') {
-            return $app->bootPath();
-        }
-
-        if ($dir == 'config') {
-            return $app->configPath();
-        }
-
-        if ($dir == 'public') {
-            return $app->publicPath();
-        }
-
-        if ($dir == 'storage') {
-            return $app->storagePath();
-        }
-
-        if ($dir == 'log') {
-            return $app->logPath();
-        }
-
-        if ($dir == 'resource') {
-            return $app->resourcePath();
-        }
-
-        if ($dir == 'template') {
-            return $app->templatePath();
-        }
-
-        return null;
+        return $app->{$dir . 'Path'}();
     }
 }
